3ba23c735abefcf2936e6570b4c33b263c66e882
Ferry Febriansyah <perriebenhard@gmail.com>
2023-12-18 16:30:48 +0700

    add meta tag

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -6,7 +6,7 @@ const Popular = () => {
   return (
     <div className="py-20 px-28 bg-slate-50">
       <h1 className="text-4xl font-semibold text-center pb-10 tracking-wide text-slate-700">
-        Produk Terlaris <span className='font-bold text-slate-800'>Alpha Clothes</span>
+        Produk Terlaris <span className="font-bold text-slate-800">Alpha Clothes</span>
       </h1>
       <div className="grid grid-cols-3 gap-10">
         {data_product.map((item, i) => {
